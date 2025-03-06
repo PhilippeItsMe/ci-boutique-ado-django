9ba97ef2c119265fa4e1ef4cd8b599f0e6865e82
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2025-03-06 10:30:34 +0000

    Security commit

--- bag/views.py
+++ bag/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
-from django.contrib import messages #To be able to display message
+from django.contrib import messages
 
 from products.models import Product
 
@@ -36,7 +36,7 @@ def add_to_bag(request, item_id):
             bag[item_id] += quantity
         else:
             bag[item_id] = quantity
-            messages.success(request, f'Added {product.name} to your bag') #Confirmation message 
+            messages.success(request, f'Added {product.name} to your bag')
 
     request.session['bag'] = bag
     return redirect(redirect_url)

e6c838f16ba4d6670d31f203bdcc13dfed953dc4
Philippe Tschanun <philippe@digitalgrowthagency.ch>
2025-03-11 14:03:10 +0000

    Ad stripe payement

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51R1SsO4CudYZNQaNoi0nz7DTMN9JU9GHDeyWqsSeArpEtmzLmeG8j7lJc64oYjx1GAK2L9DxYFG3inC6hI9YoJ2p00THhYfK2y',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
